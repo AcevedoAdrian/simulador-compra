83d3f583f786f5cd2fffabc14220e4616a6296d5
acevedo-adrian <acevedohugoadrian@gmail.com>
2021-08-16 19:10:42 -0300

    Agregando la simulacion del calculo de la finalizacion de la compra de 1 producto

--- js/main.js
+++ js/main.js
@@ -1,67 +1,100 @@
 // calcular el monto total a pagar, suponiendo que ya tiene el producto en el carrito
 let productosElegidos = 5500;
 
-
-const calcularEnvio = (costoParcial ) => {
-  let campoValido ;
-  do {
-    campoValido=true;
-    let envio = prompt('Ingrese la forma de envio: \n - Resitencia $330 \n - Corrientes $390\n - Interior (Resto del Pais) $440 \n - Retiro (Por tienda)$0').toLowerCase();
-   
-
-    switch (envio) {
-      case "resistencia":
-        costoParcial += 330;
-        break;
-      case "corrientes":
-        costoParcial += 390;
-        break;
-      case "interior":
-        costoParcial += 440;
-        break;
-      case "retiro":
-        costoParcial += 0;
-        break;
-      default: campoValido = false;
-    }
-  } while (campoValido !== true);
-
-  return costoParcial;
-};
-
-const calcularFormaDePago = (producto) =>{
-  let campoValido =  true;
-  let costoParcial = 0;
-  let total = 0;
-  do {
-    campoValido =  true;
-    let opcionPago = parseInt(prompt('Ingrese la forma de pago elgiendo la opcion: 1, 2 o 3 : \n 1- tarjeta 3 cuotas ( 15% interes) \n 2- tarjeta 6 cuotas ( 20% interes) \n 3- debito (5% descuento)'));
-   
-    switch (opcionPago) {
-      case 1:
-        costoParcial = producto + (producto * 0.15);
-        break;
-      case 2:
-        costoParcial = producto + (producto * 0.20);
-        break;
-      case 3:
-        costoParcial = producto - (producto * 0.05);
-        break;
-      default: campoValido = false;
-    }
-
-  } while (campoValido !== true);
-
-  total  =  calcularEnvio(costoParcial);
-
-  return total;
+class Producto {
+  constructor(nombre, precio, cantidad) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+  }
 
 }
 
-const sumaTotalCompra = (producto) =>{
-  alert(`El subtotal de tus productos en el carrito es de: $${producto}, a continuacion elija forma de pago y envio`);
-  let total =calcularFormaDePago(producto);
-  alert(`El total a pagar es de $${total}`);
+class Carrito {
+  constructor(producto, cantidad) {
+    this.producto = producto;
+    this.cantidad = cantidad;
+    this.precioEnvio = 0;
+    this.subTotal = 0; 
+    this.formaDePago= 0;
+    this.total = 0;
+
+  }
+
+  calcularSubTotal(){
+    this.subTotal = this.producto.precio * this.cantidad;
+  }
+
+  calcularEnvio() {
+    let campoValido;
+    do {
+      campoValido = true;
+      let envio = prompt('Ingrese la forma de envio: \n - Resitencia $330 \n - Corrientes $390\n - Interior (Resto del Pais) $440 \n - Retiro (Por tienda)$0').toLowerCase();
+
+      switch (envio) {
+        case "resistencia":
+          this.precioEnvio = 330;
+          break;
+        case "corrientes":
+          this.precioEnvio = 390;
+          break;
+        case "interior":
+          this.precioEnvio = 440;
+          break;
+        case "retiro":
+          this.precioEnvio = 0;
+          break;
+        default:
+          campoValido = false;
+      }
+    } while (campoValido !== true);
+
+  };
+
+  calcularFormaDePago()  {
+    let campoValido = true;
+    do {
+      campoValido = true;
+      let total = (this.subTotal + this.precioEnvio );
+      let opcionPago = parseInt(prompt('Ingrese la forma de pago elgiendo la opcion: 1, 2 o 3 : \n 1- tarjeta 3 cuotas ( 15% interes) \n 2- tarjeta 6 cuotas ( 20% interes) \n 3- debito (5% descuento)'));
+
+      switch (opcionPago) {
+        case 1:
+          this.formaDePago = total * 0.15 +total;
+          break;
+        case 2:
+          this.formaDePago = total * 0.20 + total;
+          break;
+        case 3:
+          this.formaDePago =  total * 0.05 - total;
+          break;
+        default:
+          campoValido = false;
+      }
+
+    } while (campoValido !== true);
+
+
+  }
+
+  compraFinalizada(){
+    alert(`La compra del producto ${this.producto.nombre} con una cantidad de ${this.producto.cantidad} tien un costo de $${this.subTotal}, sumando el envio y la forma de pago hace un total de $${this.formaDePago}`)
+  }
+
+  finalizarCompra() {
+    this.calcularSubTotal();
+    this.calcularEnvio();
+    this.calcularFormaDePago();
+    this.compraFinalizada();
+    
+  }
+
 }
 
-sumaTotalCompra(productosElegidos);
+const producto1 = new Producto('Posa Vaso', 230, 3);
+const producto2 = new Producto('Tapiz', 2300, 10);
+
+
+const compra = new Carrito(producto1, 2);
+compra.finalizarCompra();
+
